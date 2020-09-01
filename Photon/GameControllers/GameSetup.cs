4f5fbb8bffe590c253b037e4ae31ce2df75c5e2e
xh8low <64629085+xh8low@users.noreply.github.com>
2020-09-01 17:29:02 +0800

    Add files via upload
    Info Gamer, Photon Transform View with Photon PUN 2 in Unity (Part 7)(Completed)

---
+++ Photon/GameControllers/GameSetup.cs
@@ -0,0 +1,21 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class GameSetup : MonoBehaviour
+{
+    public static GameSetup GS;
+    public Transform[] spawnPoints;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        if (GameSetup.GS == null)
+        {
+            GameSetup.GS = this;
+        }
+    }
+}
